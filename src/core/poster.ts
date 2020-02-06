c260cbd1dc4698f1dc7f4b0a637aa6d703c151f5
appako <appako2603@gmail.com>
2020-02-06 15:11:54 +0200

    v 0.0.16

--- src/core/poster.ts
+++ src/core/poster.ts
@@ -1,14 +1,17 @@
 import { Menu } from "./menu/menu";
 import { Access } from "./access";
 import { Settings } from "./settings";
+import { Dash } from "./dash";
 
 export class Poster {
     menu: Menu;
     access: Access;
     settings: Settings;
+    dash: Dash;
     constructor(protected readonly token: string) {
         this.menu = new Menu(token);
         this.access = new Access(token);
         this.settings = new Settings(token);
+        this.dash = new Dash(token);
     }
 }
